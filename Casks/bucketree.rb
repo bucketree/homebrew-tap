4926d95806d9115b307035f9bad64a64e8770f83
WebUppAdmin <admin@webupp.com>
2026-09-15 19:26:16 +0100

    bucketree 1.0.0,694

---
+++ Casks/bucketree.rb
@@ -0,0 +1,23 @@
+cask "bucketree" do
+  version "1.0.0,694"
+  sha256 "61db7407bf337f89c1aae51280edc9d2830cf727f1089fb9e313b8707c229a9e"
+
+  url "https://updates.bucketree.app/Bucketree-#{version.csv.first}-#{version.csv.second}.dmg"
+  name "Bucketree"
+  desc "Native Mac manager for Backblaze B2 buckets, keys, storage and replication"
+  homepage "https://bucketree.app/"
+
+  livecheck do
+    url "https://updates.bucketree.app/appcast.xml"
+    strategy :sparkle, &:short_version
+  end
+
+  auto_updates true
+  depends_on macos: ">= :tahoe"
+
+  app "Bucketree.app"
+
+  zap trash: [
+    "~/Library/Containers/com.webupp.bucketree",
+  ]
+end
