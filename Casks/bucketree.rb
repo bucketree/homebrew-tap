8325e1e4c41ff1ee48274d810cd54f08024bc95d
WebUppAdmin <admin@webupp.com>
2026-09-17 08:52:21 +0100

    Bucketree 1.0.0 (730)

--- Casks/bucketree.rb
+++ Casks/bucketree.rb
@@ -1,6 +1,6 @@
 cask "bucketree" do
-  version "1.0.0,720"
-  sha256 "2d25b9d876aeb43306ed0f87b17e5abdc1c79409a65c0ea66546b35674301945"
+  version "1.0.0,730"
+  sha256 "03bc754b97cb25139799f8573c29e4e0090c244c66902bc3792935b1d3aef7ae"
 
   url "https://updates.bucketree.app/Bucketree-#{version.csv.first}-#{version.csv.second}.dmg"
   name "Bucketree"
